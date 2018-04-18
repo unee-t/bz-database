ca4f4782ffcf7b9c4c5008374f42e0321ce44128
franck.boullier - home <franck.boullier@gmail.com>
2018-04-18 08:50:31 +0800

    draft version of the script to update DB schema to v3.0

---
+++ db upgrade/upgrade_unee-t_v2.19_to_v3.0.sql
@@ -0,0 +1,133 @@
+# This update allows us to use lambda to send notifications out
+#
+#################################################################################
+#
+# WARNING! You MUST use Amazon Aurora database engine for this version to work!!!
+#
+#################################################################################
+
+# We create a new table 'ut_notification_messages_cases' which captures the notification information about cases.
+
+	/*!40101 SET NAMES utf8 */;
+
+	/*!40101 SET SQL_MODE=''*/;
+
+	/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+	/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+	/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+	/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+	/*Table structure for table `ut_notification_messages_cases` */
+
+	DROP TABLE IF EXISTS `ut_notification_messages_cases`;
+
+	CREATE TABLE `ut_notification_messages_cases` (
+	  `notification_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id in this table',
+	  `created_datetime` datetime DEFAULT NULL COMMENT 'Timestamp when this was created',
+	  `processed_datetime` datetime DEFAULT NULL COMMENT 'Timestamp when this notification was processed',
+	  `unit_id` smallint(6) DEFAULT NULL COMMENT 'Unit ID - a FK to the BZ table ''products''',
+	  `case_id` mediumint(9) DEFAULT NULL COMMENT 'Case ID - a FK to the BZ table ''bugs''',
+	  `user_id` mediumint(9) DEFAULT NULL COMMENT 'User ID - The user who needs to be notified - a FK to the BZ table ''profiles''',
+	  `update_what` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT 'The field that was updated',
+	  PRIMARY KEY (`notification_id`)
+	) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+	/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+	/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+	/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+	/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+# We create triggers which update the `ut_notification_messages_cases` table 
+# each time a change is made to a case
+
+# First we drop the triggers in case they already exist
+
+DROP TRIGGER IF EXISTS `ut_prepare_message_new_case`;
+DROP TRIGGER IF EXISTS `ut_prepare_message_case_activity`;
+DROP TRIGGER IF EXISTS `ut_prepare_message_new_comment`;
+
+# We then create the trigger when a case is created
+DELIMITER $$
+CREATE TRIGGER `ut_prepare_message_new_case`
+AFTER INSERT ON `bugs`
+FOR EACH ROW
+BEGIN
+	SET @unit_id = NEW.`product_id`;
+	SET @case_id = NEW.`bug_id`;
+	SET @user_id = NEW.`reporter`;
+	SET @update_what = 'New Case';
+	INSERT INTO `ut_notification_messages_cases`
+		(`created_datetime`
+		, `unit_id`
+		, `case_id`
+		, `user_id`
+		, `update_what`
+		)
+		VALUES
+		(NOW()
+		, @unit_id
+		, @case_id
+		, @user_id
+		, @update_what
+		)
+		;
+END;
+$$
+DELIMITER ;
+
+# We then create the trigger when a case is updated
+DELIMITER $$
+CREATE TRIGGER `ut_prepare_message_case_activity`
+AFTER INSERT ON `bugs_activity`
+FOR EACH ROW
+BEGIN
+	SET @unit_id = (SELECT `product_id` FROM `bugs` WHERE `bug_id` = NEW.`bug_id`);
+	SET @case_id = NEW.`bug_id`;
+	SET @user_id = NEW.`who`;
+	SET @update_what = (SELECT `description` FROM `fielddefs` WHERE `id` = NEW.`fieldid`);
+	INSERT INTO `ut_notification_messages_cases`
+		(`created_datetime`
+		, `unit_id`
+		, `case_id`
+		, `user_id`
+		, `update_what`
+		)
+		VALUES
+		(NOW()
+		, @unit_id
+		, @case_id
+		, @user_id
+		, @update_what
+		)
+		;
+END;
+$$
+DELIMITER ;
+
+# We then create the trigger when a new message is added
+DELIMITER $$
+CREATE TRIGGER `ut_prepare_message_new_comment`
+AFTER INSERT ON `longdescs`
+FOR EACH ROW
+BEGIN
+	SET @unit_id = (SELECT `product_id` FROM `bugs` WHERE `bug_id` = NEW.`bug_id`);
+	SET @case_id = NEW.`bug_id`;
+	SET @user_id = NEW.`who`;
+	SET @update_what = 'New Message';
+	INSERT INTO `ut_notification_messages_cases`
+		(`created_datetime`
+		, `unit_id`
+		, `case_id`
+		, `user_id`
+		, `update_what`
+		)
+		VALUES
+		(NOW()
+		, @unit_id
+		, @case_id
+		, @user_id
+		, @update_what
+		)
+		;
+END;
+$$
+DELIMITER ;
--- db upgrade/upgrade_unee-t_v2.9_to_v2.10.sql
+++
@@ -1,3 +0,0 @@
-# We alter the database to add an index that speeds up the login process
-
-ALTER TABLE `group_group_map` ADD INDEX `group_group_map_grant_type_member_id` (`member_id`,`grant_type`);
